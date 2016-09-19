02d2832567f67e8b6689e12bf62af095a40a2eb8
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-09-19 18:10:59 -0400

    Add numShoes and corrosponding mutator, add dispatcher

--- testcode/2016-09-19_test.js
+++ testcode/2016-09-19_test.js
@@ -1,6 +1,22 @@
+#!/usr/bin/env node
 /**
  *	@author Stewart Johnston <johnstons1@student.ncmich.edu>
- *	@version 2016-09-19
+ *	@version 2016.09.19.01
  *	@summary Test file | created 2016-09-19
  *	@todo
  */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+let numShoes;
+
+function main(){
+	setNumShoes();
+}
+
+main();
+
+function setNumShoes(){
+	numShoes = PROMPT.question('\nEnter the number of shoes: ');
+}
