ce19eadb94e994b12973058cc0bc328ab33f15bd
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-09-26 14:31:38 -0400

    Boilerplate and baseline for project: comment header block, pragmas, dispatch

---
+++ Project_1/Project_1-pg84-q10.js
@@ -0,0 +1,14 @@
+/**
+ *	@Author: Stewart Johnston <johnstons1@student.ncmich.edu>
+ *	@Version: 2016.09.26.01
+ *	@Summary: Project One, River Falls Construction Company customer lot configuration
+ *	@Todo
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+function main() {
+}
+
+main();
