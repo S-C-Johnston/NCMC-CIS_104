2ca89303c9fb7ecc6373724ae1283cc826b8095b
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-10-10 15:28:35 -0400

    Project 2 initial comment header block, pragmas, variable declarations and constant definitions.

---
+++ Project_2/Project_2.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+/**
+ *	@author: Stewart Johnston <johnstons1@student.ncmich.edu>
+ *	@Summary: Drive-rite insurance agnecy premium calculator | Created 2016-10-10
+ *	@version: 2016.10.10.01
+ *	@todo: 
+ */ 
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+let policyNumber, numAtFaultAccidents;
+let nameLast, nameFirst;
+let customerBirthdate, premiumDueDate;
+const	BASE_PRICE = 100,
+	PRICE_TWIXT_15_30 = 20,
+	PRICE_TWIXT_30_45 = 10,
+	PRICE_GT_60 = 30,
+	PRICE_PER_ACCIDENT = 50;
