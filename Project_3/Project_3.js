e8b0be733a6d087b57eba28301ce18577c11d224
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-12-13 13:04:18 -0500

    Project_3 initial commit, program @ version 1.0

---
+++ Project_3/Project_3.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+// jshint esversion: 6
+/**
+ * @author: Stewart Johnston <johnstons1@student.ncmich.edu>
+ * @summary: Hollywood Movie Rating Guide rating program | Created 2016-12-13
+ * @version: 1.0 | 1.0 deemed feature complete
+ * @todo: 
+ */
+
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+const MAX_TRIES = 3;
+const CONTINUE_YES = 1,
+      CONTINUE_NO = 0;
+let continueResponse;
+
+function main() {
+	printGreeting();
+
+	const MOVIE_TITLE = "Hello Kitty";
+	let movieRating;
+	let numRatings = 0;
+	let ratingsSum = 0;
+
+	setContinueResponse();
+
+	while (CONTINUE_YES === continueResponse) {
+		movieRating = inputMovieRating(MOVIE_TITLE);
+		numRatings++;
+		ratingsSum += movieRating;
+		setContinueResponse();
+	}
+
+	let avgRating = calcAvg();
+	calcAvg(ratingsSum, numRatings, true);
+
+	printGoodbye();
+}
+
+main();
+
+function calcAvg(setSum, numSetParts, doPrint) {
+	let averageValue = (setSum / numSetParts); //Mean average
+	if (true === doPrint) {
+		console.log(`\nThe average value is ${averageValue}.`);
+	}
+	return averageValue;
+}
+
+function inputMovieRating(movieTitle) {
+	console.log(`\nYou will be asked to rate ${movieTitle} in stars. Higher is better.`);
+	let lMovieRating = Number(inputNumericInRange(0,5));
+	console.log(`\nYou rated ${movieTitle} at ${lMovieRating} stars.`);
+
+	return lMovieRating;
+}
+
+function setContinueResponse() {
+	if (undefined !== continueResponse) {
+		continueResponse = -1;
+		while (continueResponse !== CONTINUE_NO &&
+		       continueResponse !== CONTINUE_YES) {
+			continueResponse = Number(PROMPT.question(`\nDo you want to continue? [0=no, 1=yes]: `));
+		}
+	}
+	else {
+		continueResponse = CONTINUE_YES;
+	}
+}
+
+function printGreeting() {
+	console.log(`\nWelcome to the Hollywood Movie Rating Guide rating program.`);
+}
+
+function printGoodbye() {
+	console.log(`\nThanks for using the Hollywood Movie Rating Guide rating program!`);
+}
+
+function inputNumericInRange(MIN, MAX, tries) { //returns numeric value
+	tries = (undefined === tries) ? 0 : tries + 1;
+	if (tries > MAX_TRIES) {
+		console.log(`\nPermitted number of tries exceeded. Program will now exit.`);
+		process.exit();
+	}
+
+	let lUserInput = Number(PROMPT.question(`\nPlease enter a number between ${MIN} and ${MAX}: `));
+
+	if (isNaN(lUserInput) || MIN > lUserInput || MAX < lUserInput) {
+		console.log(`\nError! The value you entered either wasn't a number or wasn't in range. Please try again.`);
+		let triesRemaining = (MAX_TRIES - tries);
+		console.log(`\nYou have ${triesRemaining} tries remaining.`);
+		return inputNumericInRange(MIN, MAX, tries);
+	}
+
+	return lUserInput;
+}
