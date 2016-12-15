d6e13c719122b7809f039699a860339c3ab4cb96
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-12-14 22:08:13 -0500

    Initial commit for Project 6, boilerplate junk

---
+++ Project_6/Project_6.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+// jshint esversion: 6
+/**
+ * @author: Stewart Johnston <johnstons1@student.ncmich.edu>
+ * @summary:
+ * @version: 
+ * @todo:
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+function main() {
+}
+
+main();
+
