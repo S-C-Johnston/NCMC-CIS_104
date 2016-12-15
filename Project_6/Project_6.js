5f96a6bce2c5331f0a98be4e8efe1878b8247593
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-12-15 00:54:56 -0500

    Project 6 comments for todos

--- Project_6/Project_6.js
+++ Project_6/Project_6.js
@@ -2,16 +2,42 @@
 // jshint esversion: 6
 /**
  * @author: Stewart Johnston <johnstons1@student.ncmich.edu>
- * @summary:
- * @version: 
+ * @summary: Curl Up and Dye record-keeper | Created 2016-12-14
+ * @version: 2016.12.14.01
  * @todo:
  */
 
 "use strict";
 const PROMPT = require('readline-sync');
+const IO = require('fs');
+
+//Master record constant
+//Master record backup constant
+//Master record format indexes
+//ID
+//First name
+//Last name
+//Transaction sum to date
+//Times coupon has been triggered
+
+//(Weekly) Transaction record constant
+//Transaction record format indexes
 
 function main() {
 }
 
 main();
 
+//Read file in and split into 2d array, return that array as a result
+
+//Sort record arrays by ID number
+
+//Match transaction record to master record, update cumulative total to
+//master record. Add current week's spent (transaction record) to master record
+//spent to date. 
+//Output: updated master record, error file when any transaction record doesn't
+//have a matching master record ID.
+//
+//Each time $750 is exceeded, output a coupon for free haircut
+
+//Allow updating transaction record
